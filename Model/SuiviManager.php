4b160ec48347c940ac8900c2f85a8362dbee80db
dreyblack <44005710+DreyBlack@users.noreply.github.com>
2019-03-06 19:11:47 +0100

    fin prospect

--- Model/SuiviManager.php
+++ Model/SuiviManager.php
@@ -100,7 +100,7 @@ class SuiviManager extends Manager
 							 <th><form action='index.php?action=validation&id=".$donnees['id']."' id=".$drapeau." method='post'>
 									<input type='checkbox' onChange='document.getElementById(".$drapeau.").submit()'></input>
 								</form>
-							 <th> <a 'index.php?action=suivi&suivi=".$Suivi."&id=".$donnees['id']."' ><button class='btn btn-s btn-warning pull-right'> Voir détails </button> </a> </th>
+							 <th> <a href='index.php?action=suivi&suivi=Commande&id=".$donnees['id']."' ><button class='btn btn-s btn-warning pull-right'> Voir détails </button> </a> </th>
 						 </tr>";
 						$drapeau = $drapeau +1;
 			}
